b6db424c6923a12e64d352bf0e3761178a2a3f50
Alexander <oleksandrlazebnij8@gmail.com>
2024-07-11 17:57:04 +0300

    added serializers for user model

--- booking_service/users/serializers.py
+++ booking_service/users/serializers.py
@@ -1,3 +1,25 @@
-from rest_framework import serial
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import RefreshToken
 
 User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'is_premium')
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'email')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data['email']
+        )
+        return user
+
